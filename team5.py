db96aff0023721bd50466e8a532c2aa9cebf5487
LaVar Ball <33550789+LaVarBall2@users.noreply.github.com>
2017-11-21 09:47:26 -0500

    Add files via upload

---
+++ team5.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+team_name = 'Коммунисты'
+strategy_name = 'return b'
+strategy_description = 'use communism to win'
+def move(my_history, their_history, my_score, their_score):
+    if 'b' in their_history or len(their_history)>100: 
+        return 'b'
+    else:
+        return 'b'
+    
+    
+    
