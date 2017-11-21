c541fa6e77d145a85e399e0835268733a0f74726
Jadon Wallace <33550755+TheChromeGamer2002@users.noreply.github.com>
2017-11-21 09:39:57 -0500

    Add files via upload

---
+++ team1.py
@@ -0,0 +1,6 @@
+team_name = 'The Quiet But Deadly'
+strategy_name = 'Only Betray (The Winners Strategy)'
+strategy_description = 'The program will only betray the opponent because that is the only way to get the maxium amount of points.'
+
+def move(my_history, their_history, my_score, their_score):
+    return 'b'
